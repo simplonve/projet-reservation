d9feae9008522faa6f1fe6fc00df8b1f7db2b327
thalion26 <thalion26@gmail.com>
2015-05-27 15:28:08 +0200

    Creation du design du formulaire pour les reservations

--- app/assets/javascripts/reservation.js
+++ app/assets/javascripts/reservation.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+$(document).ready(function() {
+	$('.input__field, .textarea').blur(function() {
+		if ( $(this).val() != "" ) {
+			$(this).next().addClass('filled');
+		}
+		else {
+			$(this).next().removeClass('filled');
+		}
+	});
+})
