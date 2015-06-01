4477100f7578e93954cb1d68759dced47e4e5498
p-guerin <guerin.pierre26@gmail.com>
2015-06-01 09:35:40 +0200

    Refactoring du code du calendrier de index.erb

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ class Intervenant < ActiveRecord::Base
 end
 
 get "/" do
+  @m = {6=>["Juin", 30, 1], 7=>["Juillet", 31, 3], 8=>["Août", 31, 6], 9=>["Septembre", 30, 2], 10=>["Octobre", 31, 4]}
+  @d = ["Lundi", "Mardi", "Mercredi" , "Jeudi", "Vendredi", "Samedi", "Dimanche"]
   @intervenant = Intervenant.all
   @title = "Intervenants"
   erb :index
